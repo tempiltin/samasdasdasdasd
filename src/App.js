933e310c04e1d0f21d706bec04ed0187663f5df5
__________ <temurbekshukurov0707@gmail.com>
2022-05-24 18:14:35 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from "react";
 function App() {
-  useEffect(()=> {
-    Aos.init({duration:2000})
-  },[])
+  useEffect(()=> {Aos.init({duration:2000})},[])
   return (
     <>
       <NavbarTop />
--- src/Pages/HomeENG.js
+++ src/Pages/HomeENG.js
@@ -4,9 +4,7 @@ import Header from '../Components/ENG/HeaderTop/Header'
 import Strategy from '../Components/ENG/Strategy/Strategy'
 import Portfolio from '../Components/Portfolio/Portfolio'
 import Services from '../Components/Services/Services'
-
 const HomeENG = () => {
- 
   return (
     <>
     <Header />
