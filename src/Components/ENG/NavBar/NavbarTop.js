742c364edf64907aa249e14a9ae05f099236e416
__________ <temurbekshukurov0707@gmail.com>
2022-05-30 09:09:57 +0500

    upload

--- src/Components/ENG/NavBar/NavbarTop.js
+++ src/Components/ENG/NavBar/NavbarTop.js
@@ -1,61 +1,62 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import img from '../../../assets/favicon.png'
-import {FaHeadphonesAlt} from 'react-icons/fa'
+import { FaHeadphonesAlt } from 'react-icons/fa'
 import './navBar.scss'
 const NavbarTop = () => {
-  window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () { scrollFunction() };
   function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("navbar").style.padding = "10px 10px";
       document.getElementById("navbar").style.boxShadow = " 0 13px 35px -12px rgba(35, 35, 35 , 10%)";
-  
+
     } else {
       document.getElementById("navbar").style.padding = "30px 10px";
       document.getElementById("navbar").style.boxShadow = " 0 13px 35px -12px #ffff";
-   
+
     }
   }
   return (
     <>
-    
+
       <nav id='navbar' className="navbar navbar-light">
         <div className="container">
           <Link className="navbar-brand d-flex" to='/'>
-            <img src={img} alt="png" className='NavbarIMG'/> <strong className='d-flex d-lg-none NavStrong'>rogrammers <span> UZ</span></strong>
+            <img src={img} alt="png" className='NavbarIMG' />
+            <strong className='d-flex d-lg-none NavStrong'>Programmers <span> UZ</span></strong>
           </Link>
-        
-           <ul className='NavUL d-none d-lg-flex'>
-             <li><Link to="/" className='active'>Home</Link> </li>
-             <li><a href="#portfolio">Portfolio</a></li>
-             <li><a href="#services">Services</a></li>
-             <li><Link to="/contact">Contact</Link> </li>
-             <li><Link to="/blog">Blog</Link> </li>
-           </ul>
-           <ul className='NavUL d-none d-lg-flex'>
-             <li ><a href="tel:662300015"><FaHeadphonesAlt className='NavIcon'/> <span> +998 (66) 230 00 15</span></a></li>
-           </ul>
+
+          <ul className='NavUL d-none d-lg-flex'>
+            <li><Link to="/" className='active'>Home</Link> </li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><Link to="/contact">Contact</Link> </li>
+            <li><Link to="/blog">Blog</Link> </li>
+          </ul>
+          <ul className='NavUL d-none d-lg-flex'>
+            <li ><a href="tel:662300015"><FaHeadphonesAlt className='NavIcon' /> <span> +998 (66) 230 00 15</span></a></li>
+          </ul>
 
           <button className=" navbar-toggler d-lg-none d-md-block d-block" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="offcanvas offcanvas-end"  id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas offcanvas-end" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
-              
+
               <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              
-             <li><Link to="/" className='active'>Home</Link> </li>
-             <li><a href="#portfolio">Portfolio</a></li>
-             <li><a href="#services">Services</a></li>
-             <li><Link to="/contact">Contact</Link> </li>
-             <li><Link to="/blog">Blog</Link> </li>
-    
-           <ul className='NavUL '>
-             <li ><a href="tel:662300015"><FaHeadphonesAlt className='NavIcon'/> <span> +998 (66) 230 00 15</span></a></li>
-           </ul>
+
+                <li><Link to="/" className='active'>Home</Link> </li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><Link to="/contact">Contact</Link> </li>
+                <li><Link to="/blog">Blog</Link> </li>
+
+                <ul className='NavUL '>
+                  <li ><a href="tel:662300015"><FaHeadphonesAlt className='NavIcon' /> <span> +998 (66) 230 00 15</span></a></li>
+                </ul>
               </ul>
 
             </div>
--- src/Components/Portfolio/Porfolio1.jsx
+++ src/Components/Portfolio/Porfolio1.jsx
@@ -6,8 +6,6 @@ const Porfolio1 = () => {
     const [cardData, setCordData] = useState([]);
     const [visible, setVisible] = useState([4])
     const [urllink, setUrlLink] = useState("https://companyyy.herokuapp.com/api/all_product/")
-
-
     const allCardData = async () => {
         const response = await axios.get(`${urllink}`)
         setUrlLink(response.data.next)
@@ -18,15 +16,12 @@ const Porfolio1 = () => {
     const loadMore = () => {
         setVisible(visible + 2)
         allCardData()
-
     }
     useEffect(() => {
         allCardData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     const renderCard = (person, index) => {
-        // console.log(person);
-        console.log("hello world");
         return (
             <div className="col-lg-6 col-md-6" key={index}>
                 <div data-aos="zoom-in-down" className="portDivImg">
@@ -42,7 +37,6 @@ const Porfolio1 = () => {
     }
     return (
         <section id="portfolio">
-
             <div className="container">
                 <div className="row">
                     <div className='col-lg-5 col-md-6 col-sm-8'>
@@ -54,23 +48,14 @@ const Porfolio1 = () => {
             </div>
             <div className="container">
                 <div className="row" >
-
-                    {cardData.slice(0, visible).map((card,index) => {
-                        console.log(card);
-                    return  renderCard(card,index)
+                    {cardData.slice(0, visible).map((card, index) => {
+                        return renderCard(card, index)
                     })}
-
-
                 </div>
-                <div className="row">
+                <div className="row mt-5">
                     <div className="col-12">
                         <div className="Drop">
-
-                            {/* {visible < cardData.length && (
-                                )} */}
-
-                            <button onClick={loadMore}>Load More</button>
-
+                            <button onClick={loadMore} className=" PortfolioButton">Load More</button>
                         </div>
                     </div>
                 </div>
